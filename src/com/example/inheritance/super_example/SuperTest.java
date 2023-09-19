fdad5231cc5566794933a0173fdcb3961ca59298
test1 <vascoosee@wp.pl>
2023-09-19 13:01:19 +0200

    test2 seba

--- src/com/example/inheritance/super_example/SuperTest.java
+++ src/com/example/inheritance/super_example/SuperTest.java
@@ -2,9 +2,9 @@ package com.example.inheritance.super_example;
 
 public class SuperTest {
     public static void main(String[] args) {
-        Student student = new Student("Ania", "Kowalska", 5);
+        Student student = new Student("Ania", "Kowalskaiii", 5);
         Teacher teacher = new Teacher("Adam", "dobry", "math");
-
+        //Director director = new Director("Seba", "Wazy", 2);
 
         System.out.println(teacher.getTeachingSubject());
     }
